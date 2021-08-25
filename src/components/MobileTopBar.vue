6005c7a85243cd8689101cf62813c8b36f2ccf74
hsinhui <shinhuei1@gmail.com>
2021-08-25 11:06:23 +0800

    add mobile top bar

--- src/components/MobileTopBar.vue
+++ src/components/MobileTopBar.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
-    <v-sheet>
-      123
+    <v-sheet class="pa-2 d-flex align-center">
+      <router-link :to="backPath">
+        <v-icon>mdi-chevron-left</v-icon>
+        回上頁
+      </router-link>
+      <div class="text-h6 title-center">
+        {{ title }}
+      </div>
     </v-sheet>
+    <v-divider />
   </div>
 </template>
 <script>
 export default {
-  name: "",
-  data() {
-    return {};
+  name: "MobileTopBar",
+  props: {
+    title: {
+      type: String
+    },
+    backPath: {
+      type: Object
+    }
   }
 };
 </script>
+<style lang="scss" scoped>
+.title-center {
+  width: 65%;
+  text-align: center;
+}
+</style>
