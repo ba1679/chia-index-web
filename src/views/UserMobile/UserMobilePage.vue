5001945b354fd8fa4bf99a10300c8df326aaeaf9
hsinhui <shinhuei1@gmail.com>
2021-08-25 11:06:09 +0800

    add mobile router transition

--- src/views/UserMobile/UserMobilePage.vue
+++ src/views/UserMobile/UserMobilePage.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <router-view></router-view>
+    <transition name="mobile-router">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 <script>
@@ -8,3 +10,28 @@ export default {
   name: "UserMobilePage"
 };
 </script>
+<style lang="scss" scoped>
+.mobile-router-enter-active {
+  animation: coming 0.5s;
+}
+.mobile-router-leave-active {
+  animation: going 0.5s;
+}
+
+@keyframes going {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+}
+@keyframes coming {
+  from {
+    transform: translateX(-50%);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+</style>
