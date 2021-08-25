86d86ca51d6aab07cbac80fe75c0493a2864850e
hsinhui <shinhuei1@gmail.com>
2021-08-25 10:08:21 +0800

    set user-account page

---
+++ src/components/MobileTopBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-sheet>
+      123
+    </v-sheet>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  }
+};
+</script>
---
+++ src/views/UserMobile/UserOrdersMobile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <MobileTopBar />
+    訂單
+  </div>
+</template>
+<script>
+import MobileTopBar from "@/components/MobileTopBar";
+
+export default {
+  name: "",
+  components: {
+    MobileTopBar
+  },
+  data() {
+    return {};
+  }
+};
+</script>
