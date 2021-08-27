9e8287dc12d7d6c397873803d7d9495508e070b2
hsinhui <shinhuei1@gmail.com>
2021-08-27 16:52:23 +0800

    add new address form

---
+++ src/views/ConsumerMobile/NewAddressForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <MobileTopBar
+      :title="'新增收件地址'"
+      :backPath="{ name: 'AddressMobile' }"
+      :actionText="'儲存'"
+      @action="save"
+    />
+  </div>
+</template>
+<script>
+import MobileTopBar from "@/components/MobileTopBar";
+
+export default {
+  name: "NewAddressForm",
+  components: {
+    MobileTopBar
+  },
+  data() {
+    return {};
+  }
+};
+</script>
