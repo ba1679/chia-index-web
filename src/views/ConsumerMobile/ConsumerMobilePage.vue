bcbdbd04b6958b6ec012f6279fd29858e11247ef
hsinhui <shinhuei1@gmail.com>
2021-08-31 13:29:40 +0800

    adjust router transition

--- src/views/ConsumerMobile/ConsumerMobilePage.vue
+++ src/views/ConsumerMobile/ConsumerMobilePage.vue
@@ -11,27 +11,22 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.mobile-router-enter {
+  transform: translateX(50%);
+}
 .mobile-router-enter-active {
-  animation: coming 0.5s;
+  transition: all 0.5s;
+}
+.mobile-router-enter-to {
+  transform: translateX(0);
+}
+.mobile-router-leave {
+  transform: translateX(-30%);
 }
 .mobile-router-leave-active {
-  animation: going 0.5s;
+  transition: all 0.5s;
 }
-
-@keyframes going {
-  from {
-    transform: translateX(0);
-  }
-  to {
-    transform: translateX(-100%);
-  }
-}
-@keyframes coming {
-  from {
-    transform: translateX(-50%);
-  }
-  to {
-    transform: translateX(0);
-  }
+.mobile-router-leave-to {
+  transform: translateX(-100%);
 }
 </style>
